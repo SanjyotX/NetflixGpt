80e0e3948216227957c4e70710bebad400511224
Projects-demo <sanjyotisonawane5515@gmail.com>
2026-04-07 20:56:49 +0530

    Added components and utils folder

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,13 +4,43 @@ const Login = () =>{
     return <div>
         <Header />
         <div className="absolute">
-            <img src="https://assets.nflxext.com/ffe/siteui/vlv3/fc9ebadf-d77a-4f9b-a0dd-499af5178dc5/web/IN-en-20260330-TRIFECTA-perspective_81d2307f-4ee1-416d-8721-ac2be7b2da1d_large.jpg" alt="Netflix Background" />
-        </div>
-        <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0">
-            <h1 className="text-white font-bold text-3xl py-4">Sign In</h1>
-            <input type="text" placeholder="Email Address" className="p-2 my-2"/>
-            <input type="password" placeholder="Password" className="p-2 my-2"/>
-            <button className="bg-red-600 text-white p-4 my-4 w-full">Sign In</button>
+        <form>
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now."}
+        </p>
         </form>
     </div>;
 };
